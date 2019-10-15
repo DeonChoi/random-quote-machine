67ba6dbbb8acd6aa7ae151cf12812ccbab772b51
Deon Choi <deon.choi@gmail.com>
2019-10-15 15:59:19 -0700

    Updates

---
+++ precache-manifest.6fa0990854d07c2d86065ec8de2cebcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88674c27dc7f2b55ca3bdb4904db77a5",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "ba3b51daa46c731edc8b",
+    "url": "/random-quote-machine/static/css/main.cbb632e6.chunk.css"
+  },
+  {
+    "revision": "a7805915fc8ab1f7b576",
+    "url": "/random-quote-machine/static/js/2.0eca0035.chunk.js"
+  },
+  {
+    "revision": "ba3b51daa46c731edc8b",
+    "url": "/random-quote-machine/static/js/main.cf6eaf84.chunk.js"
+  },
+  {
+    "revision": "64d257269d5c2786ef7d",
+    "url": "/random-quote-machine/static/js/runtime-main.1b07de8c.js"
+  }
+]);
